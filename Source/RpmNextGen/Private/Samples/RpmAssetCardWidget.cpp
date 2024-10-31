69be02c3b9d27f041c35c4055a31f6b3802178bc
Harrison <harrisonhough4@gmail.com>
2024-10-31 07:03:56 +0200

    chore: update copyright notice

--- Source/RpmNextGen/Private/Samples/RpmAssetCardWidget.cpp
+++ Source/RpmNextGen/Private/Samples/RpmAssetCardWidget.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Ready Player Me
 
 #include "Samples/RpmAssetCardWidget.h"
 #include "RpmTextureLoader.h"
