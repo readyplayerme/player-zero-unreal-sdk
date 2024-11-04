77c69b48a5708f3e1993dfb35b52c63644bc04f5
Harrison <harrisonhough4@gmail.com>
2024-11-04 13:21:30 +0200

    fix: add quoted path to allow for spaces

--- Source/RpmNextGen/Private/Api/Files/PakFileUtility.cpp
+++ Source/RpmNextGen/Private/Api/Files/PakFileUtility.cpp
@@ -14,7 +14,9 @@ const FString FPakFileUtility::CachePakFilePath = FPaths::ProjectDir() / TEXT("C
 void FPakFileUtility::CreatePakFile(const FString& PakFilePath)
 {
     const FString UnrealPakPath = FPaths::ConvertRelativePathToFull(FPaths::EngineDir() / TEXT("Binaries/Win64/UnrealPak.exe"));
-    const FString CommandLineArgs = FString::Printf(TEXT("%s -Create=%s -IoStore"), *PakFilePath, *ResponseFilePath);  // Add the -IoStore flag
+    const FString QuotedPakFilePath = FString::Printf(TEXT("\"%s\""), *PakFilePath);
+    const FString QuotedResponseFilePath = FString::Printf(TEXT("\"%s\""), *ResponseFilePath);
+    const FString CommandLineArgs = FString::Printf(TEXT("%s -Create=%s -IoStore"), *QuotedPakFilePath, *QuotedResponseFilePath);  // Add the -IoStore flag
     FProcHandle ProcHandle = FPlatformProcess::CreateProc(*UnrealPakPath, *CommandLineArgs, true, false, false, nullptr, 0, nullptr, nullptr);
 
     if (ProcHandle.IsValid())
