b70de1dc53599f56571ebaa9464bce795b4afb88
Harrison Hough <harrisonhough4@gmail.com>
2025-07-27 21:30:30 +0300

    add load image function

--- Source/PlayerZero/Public/PlayerZeroFunctionLibrary.h
+++ Source/PlayerZero/Public/PlayerZeroFunctionLibrary.h
@@ -4,11 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
+#include "Kismet/GameplayStatics.h"
 #include "PlayerZeroFunctionLibrary.generated.h"
 
 class UImage;
 class UPlayerZeroSubsystem;
 
+DECLARE_DYNAMIC_DELEGATE_OneParam(FOnAvatarImageLoaded, const UImage*, AvatarImage);
 /**
  * 
  */
@@ -18,6 +20,10 @@ class PLAYERZERO_API UPlayerZeroFunctionLibrary : public UBlueprintFunctionLibra
 	GENERATED_BODY()
 
 public:
-	UFUNCTION(BlueprintCallable, Category = "PlayerZero|Avatar", meta = (WorldContext = "WorldContextObject"))
+	UFUNCTION(BlueprintCallable, Category = "PlayerZero")
 	static FString GetHotLoadedAvatarId(UObject* WorldContextObject);
+
+	UFUNCTION(BlueprintCallable, Category = "PlayerZero|Avatar", meta = (WorldContext = "WorldContextObject"))
+	static void LoadAvatarImage(UObject* WorldContextObject, const FString& AvatarId, const FVector2D& ImageSize, UImage* Image);
+
 };
